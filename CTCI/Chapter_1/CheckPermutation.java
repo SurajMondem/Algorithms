75ee0fdb361a3518a0e68e2aedfa65ae313586f4
SurajMondem <surajmondem@gmail.com>
2020-09-22 16:48:50 -0400

    Question 1_2 completed

--- CTCI/Chapter_1/CheckPermutation.java
+++ CTCI/Chapter_1/CheckPermutation.java
@@ -1,8 +1,8 @@
 import java.util.*;
 public class CheckPermutation {
     public static void main(String[] args) {
-        String[] input1 = {"Permutation", "Abaaabac", "bbdoanv"};
-        String[] input2 = {"mut", "aabc", "bdoa"};
+        String[] input1 = {"mtu", "dog", "aba", "abba"};
+        String[] input2 = {"mut", "god", "baa", "abbc"};
 
         for (int i = 0; i < input1.length; i++) {
             System.out.println(Check_Permutation(input1[i], input2[i]));
@@ -10,9 +10,22 @@ public class CheckPermutation {
 
     }
     private static boolean Check_Permutation(String string1, String string2){
-        
+        if(string1.length() != string2.length()){
+            return false;
+        }
 
+        int[] letters = new int[128];
+        for (int i = 0; i < string1.length(); i++) {
+            letters[string1.charAt(i)]++;
+        }
 
-        return false;
+        for (int i = 0; i < string2.length(); i++) {
+            letters[string2.charAt(i)]--;
+            if(letters[string2.charAt(i)] < 0){
+                return false;
+            }
+        }
+        return true;
+
     }
 }
