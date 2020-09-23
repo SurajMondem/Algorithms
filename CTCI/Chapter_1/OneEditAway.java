5006bc48a575ff13a14bd17403a6495398688c62
SurajMondem <surajmondem@gmail.com>
2020-09-23 14:51:26 -0400

    Question 1_5

---
+++ CTCI/Chapter_1/OneEditAway.java
@@ -0,0 +1,42 @@
+import java.util.*;
+public class OneEditAway {
+    public static void main(String[] args) {
+        System.out.println(checkOneEdit("pale", "ple"));
+        System.out.println(checkOneEdit("pale", "bale"));
+        System.out.println(checkOneEdit("pales", "pale"));
+        System.out.println(checkOneEdit("pale","bake"));
+
+    }
+
+    private static boolean checkOneEdit(String first, String second){
+        if(Math.abs(first.length() - second.length()) > 1){
+            return false;
+        }
+
+        String s1 = first.length() < second.length() ? first : second;
+        String s2 = first.length() < second.length() ? second : first;
+
+        int index1 = 0;
+        int index2 = 0;
+        boolean found = false;
+        while (index1 < s1.length() && index2 < s2.length()) 
+        {
+            if(s1.charAt(index1) != s2.charAt(index2)){
+                if(found){
+                    return false;
+                }
+                found = true;
+
+                if(s1.length() == s2.length()){
+                    index1++;
+                }
+            }
+            else {
+                index1++;
+            }
+            index2++;
+        }
+
+        return true;
+    }
+}
